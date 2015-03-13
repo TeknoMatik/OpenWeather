da3221a92dcd92dc003c6b89a0dcc482276f9909
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2015-03-13 23:27:28 +0600

    [API] add the openweather api key

--- app/src/main/java/com/gaifullin/rustam/openweather/server/requests/BaseRequest.java
+++ app/src/main/java/com/gaifullin/rustam/openweather/server/requests/BaseRequest.java
@@ -1,10 +1,13 @@
 package com.gaifullin.rustam.openweather.server.requests;
 
+import com.gaifullin.rustam.openweather.Constants;
 import com.loopj.android.http.RequestParams;
 import java.io.Serializable;
 
 public abstract class BaseRequest implements Serializable {
   private static final long serialVersionUID = 4252208824252727981L;
 
-  public abstract void fillParams(RequestParams params);
+  protected void fillParams(RequestParams params) {
+    params.put("APPID", Constants.API_KEY);
+  }
 }
