7aafafe10ba1af56b88a9f9722341d385564dc66
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2015-03-16 21:26:23 +0600

    change default city to Karaganda

--- app/src/main/java/com/rustam/openweather/Constants.java
+++ app/src/main/java/com/rustam/openweather/Constants.java
@@ -2,7 +2,7 @@ package com.rustam.openweather;
 
 public final class Constants {
   public static final String IMAGE_URL = "http://openweathermap.org/img/w/%s.png";
-  public static final String DEFAULT_CITY = "Chicago";
+  public static final String DEFAULT_CITY = "Karagandy";
   public static final int GPS_UPDATE_TIME = 1 * 60 * 1000;
   public static final String API_KEY = "c0d7c919cd73b4b08fcdfd92499291d9";
 }
